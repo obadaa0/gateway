d0a155a9532f5c4bd8827f43f54d102cdd5e068b
Obada Sabbagh <badasabbagh@gmail.com>
2025-06-09 20:25:05 +0300

    fi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,9 +27,9 @@ return new class extends Migration
             $table->string('phone');
             $table->boolean('block')->default(false);
             $table->string('bio')->nullable();
-            $table->string('country');
-            $table->string('city');
-            $table->string('street');
+            $table->string('country')->default('syria');
+            $table->string('city')->default('damascus');
+            $table->string('street')->default('babAlhara');
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
